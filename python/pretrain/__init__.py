0b2841e94b96bfa803d1d294ac88fec4918a792a
Alex Moriarty <amoriarty14@gmail.com>
2025-11-06 21:10:05 +0000

    Export module contents

--- python/pretrain/__init__.py
+++ python/pretrain/__init__.py
@@ -5,9 +5,26 @@ This package contains different pre-training strategies that can be used
 before self-play training to give the model a better starting point.
 """
 
-from .eval_matching import generate_eval_matching_data, pretrain_eval_matching
+from .eval_matching import (
+    generate_eval_matching_data,
+    pretrain_eval_matching,
+    save_eval_data,
+    load_eval_data,
+    generate_random_positions_branching,
+)
+from .human_games import (
+    generate_human_games_data,
+    HumanGamesDataset,
+    train_epoch_streaming,
+)
 
 __all__ = [
     'generate_eval_matching_data',
     'pretrain_eval_matching',
+    'save_eval_data',
+    'load_eval_data',
+    'generate_random_positions_branching',
+    'generate_human_games_data',
+    'HumanGamesDataset',
+    'train_epoch_streaming',
 ]
